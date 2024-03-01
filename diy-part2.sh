daede2d6f430f940ebd404c982eb8bd7d76673b7
Rym Fred <157193628+RymFred11@users.noreply.github.com>
2024-03-01 18:04:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
+git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
+git clone https://github.com/RymFred11/luci-app-ua2f.git package/luci-app-ua2f
+git clone https://github.com/RymFred11/luci-app-mac.git package/luci-app-mac
+git clone https://github.com/RymFred11/luci-app-autoshell.git package/luci-app-autoshell
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
